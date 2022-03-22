d570f6e56e3e2cffc45ee87cb2cd9ad1411cbe70
Braelan Rollins Creswell <braelan.creswell@maine.edu>
2022-03-22 12:55:19 -0400

    Added userspace files

---
+++ userspace/userspace.c
@@ -0,0 +1,16 @@
+//Braelan Creswell
+//Userspace control code
+#include <stdio.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <stdlib.h>
+
+#include "userspace.h"
+
+int main ( int argc, char *argv[] )
+{
+
+}
---
+++ userspace/userspace.h
@@ -0,0 +1,6 @@
+//Braelan Creswell
+//Userspace control header
+#ifndef USERSPACE_H
+#define USERSPACE_H
+
+#endif
