dbb1c285ed73d04b5a154fa95d5461c6f5287c9e
Braelan Creswell <braelan.creswell@maine.edu>
2022-04-03 22:05:52 +0100

    Added comments to userspace file

--- userspace/user_lcd.c
+++ userspace/user_lcd.c
@@ -14,52 +14,52 @@ int main ( int argc, char *argv[] )
 {
 	
 	if (argc < 2) {
-		printf("Usage: ./user_lcd <command> <args>\n");
+		printf("Usage: ./user_lcd <command> <args>\n"); //Make sure there are atleast 2 arguments to the program
 	    return -1;
     }
 	
-	int lcd = open("/dev/lcd", O_WRONLY);
-	if(lcd == -1) {
-		printf("Opening was not possible!\n");
+	int lcd = open("/dev/lcd", O_WRONLY); //Open device file
+	if(lcd == -1) {                       //Check for errors opening file
+		printf("Opening was not possible!\n"); 
 		return -1;
 	}
 
-    switch (atoi(argv[1])) {
+    switch (atoi(argv[1])) { //Use first CL argument as command switch
     case 2:	
-	    ioctl(lcd, LCDIO_CLEAR);
+	    ioctl(lcd, LCDIO_CLEAR); //Clear LCD IOCTL
 	    printf("Clearing LCD\n");
 		break;
     case 1:
-	    ioctl(lcd, LCDIO_INIT);
+	    ioctl(lcd, LCDIO_INIT); //INIT LCD IOCTL
 	    printf("Initializing LCD\n");
 		break;
 	case 3:
-	    write(lcd, argv[2], strlen(argv[2]));
+	    write(lcd, argv[2], strlen(argv[2])); //Write to display using 2nd CL argument as data
 	    printf("Writing to LCD\n");
 		break;
 	case 4:
-	    ioctl(lcd, LCDIO_OFF);
+	    ioctl(lcd, LCDIO_OFF); //LCD Off IOCTL
 	    printf("Turning Off LCD\n");
 		break;
 	case 5:
-	    ioctl(lcd, LCDIO_ON);
+	    ioctl(lcd, LCDIO_ON); //LCD On IOCTL
 	    printf("Turning On LCD\n");
 		break;
 	case 6:
-	    ioctl(lcd, LCDIO_CURSOR1);
+	    ioctl(lcd, LCDIO_CURSOR1); //Place Cursor on 1st line IOCTL
 	    printf("Setting Cursor 1\n");
 		break;
 	case 7:
-	    ioctl(lcd, LCDIO_CURSOR2);
+	    ioctl(lcd, LCDIO_CURSOR2); //Place Cursor on 2nd line IOCTL
 	    printf("Setting Cursor 2\n");
 		break;
 	case 8:
-	    printf("Testing Locks\n");
-		fork();
-		write(lcd, "Fork ", strlen("Fork "));
+	    printf("Testing Locks\n"); //Test locking in kernel driver
+		fork(); //fork process
+		write(lcd, "Fork ", strlen("Fork ")); //both parent and child process send write to kernel driver, proper function is 2 "Fork " displayed on screen
 		break;
 	default:
-	    printf("Command not found\n");
+	    printf("Command not found\n"); //Give user list of commands if incorrect command entered
 		printf("1: Init LCD\n");
 		printf("2: Clear LCD\n");
 		printf("3 <string>: Write to LCD\n");
